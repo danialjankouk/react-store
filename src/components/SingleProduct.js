94086a31099ef307463deb6af04e2a70ab45c492
danialjankouk <dani.jankook@gmail.com>
2022-02-01 20:51:24 +0330

    single product counter done.

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -4,18 +4,50 @@ import Iphone11 from "../images/iphone11.jpg";
 import up from "../images/up.svg";
 import down from "../images/down.svg";
 class SingleProduct extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  upHandler = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+  downHandler = () => {
+    if (this.state.count >= 1) {
+      this.setState((prevState) => ({
+        count: prevState.count - 1,
+      }));
+    }
+  };
   render() {
+    const { count } = this.state;
+    const { name, price, image } = this.props;
     return (
       <div className={Styles.product}>
         <div className={Styles.imgProduct}>
-          <img src={Iphone11} alt="phone" />
+          <img src={image} alt="phone" />
         </div>
-        <h1>iphone11</h1>
-        <p className={Styles.price}>500</p>
+        <h1>{name}</h1>
+        <p className={Styles.price}>
+          {price} {count ? "=>" : ""} {count ? `${count * Number(price.split(" ")[0])} $` : ""}
+        </p>
         <div className={Styles.counter}>
-          <img src={up} alt="up arrow" className={Styles.upArrow} />
-          <p className={Styles.count}>0</p>
-          <img src={down} alt="down arrow" className={Styles.downArrow} />
+          <img
+            src={up}
+            alt="up arrow"
+            onClick={this.upHandler}
+            className={Styles.upArrow}
+          />
+          <p className={Styles.count}>{count}</p>
+          <img
+            src={down}
+            alt="down arrow"
+            onClick={this.downHandler}
+            className={Styles.downArrow}
+          />
         </div>
       </div>
     );
